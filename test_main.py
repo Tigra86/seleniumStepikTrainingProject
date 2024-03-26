c35f2498d6feb2f4f29129fd6679207ae2a10958
Tigran <tigra.azaryan@me.com>
2024-03-26 13:49:50 -0700

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://google.com')
+time.sleep(5)
+
+driver.get('https://facebook.com')
+time.sleep(5)
+
+driver.back()
+time.sleep(5)
+
+driver.forward()
+time.sleep(5)
+
+driver.refresh()
