6be1fe0191020fdfed22d7ae4dfff60810ad132e
Tigran <tigra.azaryan@me.com>
2024-04-23 21:44:51 -0700

    Update lesson_22_action_chains.py file Add lesson_23_js.py and scrolls.py files

---
+++ lesson_23_js.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, "
+                            "like Gecko) Version/9.0.2 Safari/601.3.9")
+
+service = Service(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("https://seiyria.com/bootstrap-slider/")
+
+driver.execute_script("alert('Hello World')")  # Вызов js-кода на странице
+
+time.sleep(5)
